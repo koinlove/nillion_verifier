06aa9231d111d1606a7005164eec583d51999b94
코인러브미순 <pleasediechinese@gmail.com>
2025-04-12 13:50:40 +0900

    ㅇㄻㄹㅇㅁ

---
+++ eclipse.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+BOLD='\033[1m'
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+CYAN='\033[36m'
+MAGENTA='\033[35m'
+NC='\033[0m'
+
+# 한국어 체크하기
+check_korean_support() {
+    if locale -a | grep -q "ko_KR.utf8"; then
+        return 0  # Korean support is installed
+    else
+        return 1  # Korean support is not installed
+    fi
+}
+
+# 한국어 IF
+if check_korean_support; then
+    echo -e "${CYAN}한글있긔. 설치 넘기긔.${NC}"
+else
+    echo -e "${RED}한글없긔. 설치하겠긔.${NC}"
+    sudo apt-get install language-pack-ko -y
+    sudo locale-gen ko_KR.UTF-8
+    sudo update-locale LANG=ko_KR.UTF-8 LC_MESSAGES=POSIX
+    echo -e "${GREEN}설치 완료했긔.${NC}"
+fi
+
+setup () {
+echo -e "${RED} 꼭 우분투 24.04 버젼으로 하렴. 24.04 버젼이 아니면 이 커맨드는 먹히지도 않는단당.. ${NC}"
+#업데이트 하는 법 모르면 그냥 그대로 죽어
+
+echo -e "${CYAN}sudo apt update${NC}"
+sudo apt update
+
+echo -e "${CYAN}sudo apt upgrade -y${NC}"
+sudo apt upgrade -y
+
+echo -e "${CYAN}sudo apt autoremove -y${NC}"
+sudo apt autoremove -y
+
+echo -e "${BOLD}${CYAN} 기본 파일들을 설치합니다. ${NC}"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+echo -e "${BOLD}${CYAN} 변경사항을 적용합니다. ${NC}"
+source $HOME/.cargo/env
+
+echo -e "${BOLD}${CYAN} 솔라나 클라이언트 설치합니다*지갑 생성*${NC}"
+echo -e "${BOLD}${RED}오래 걸리니까 기다려 주세요${NC} ${MAGENTA}♥${NC}"
+curl --proto '=https' --tlsv1.2 -sSfL https://solana-install.solana.workers.dev | bash
+
+echo -e "${BOLD}${CYAN} 솔라나 지갑을 생성하겠습니다. 님의 노드 채굴 지갑이니까 잘 저장해 두세욤.${NC}"
+solana-keygen new
+
+echo -e "${BOLD}${CYAN} 여기다 돈 좀 넣어두삼. 0.005 이더리움정도.. 이클립스 메인넷으로 넣어야 됨. ${NC}"
+
+echo -e "${BOLD}${CYAN} 1분의 시간을 주겠음. ${NC}"
+sleep 60
+
+echo -e "${BOLD}${CYAN} 이제 채굴기 설치할게요~ 오래걸림 ${NC}"
+cargo install bitz
+
+echo -e "${BOLD}${CYAN} 노드의 RPC를 이클립스 메인넷으로 설정할게욤 ${NC}"
+solana config set --url https://mainnetbeta-rpc.eclipse.xyz/
+
+echo -e "${BOLD}${CYAN}이제 스크린 켜서 관리하삼. [screen -S eclipse] 입력 후 [bitz collect] 시작${NC}"
+}
+
+Bitz_command() {
+echo -e "${CYAN} Bitz 클레임하고 싶으면 [bitz claim]${NC}"
+echo -e "${CYAN} Bitz 계좌? 보고 싶으면 [bitz account]${NC}"
+echo -e "그 외에는 [bitz -h]로 명령어 확인하삼"
+}
+# 메인 메뉴
+echo && echo -e "${BOLD}${MAGENTA} ePOW..? 이름 존나 구림 ㅉ 암튼 그거 캐는 노드 스크립트 ${NC} by 코인러브미순
+ ${CYAN}원하는 거 고르시고 실행하시고 그러세요. ${NC}
+ ———————————————————————
+ ${GREEN} 1. 기본파일 설치 및 ePOW 세팅 및 키 생성기 ${NC}
+ ${GREEN} 2. BITZ 관련 명령어 열람 ${NC}
+ ———————————————————————" && echo
+
+# 사용자 입력 대기
+echo -ne "${BOLD}${MAGENTA} 어떤 작업을 수행하고 싶으신가요? 위 항목을 참고해 숫자를 입력해 주세요: ${NC}"
+read -e num
+
+case "$num" in
+1)
+    setup
+    ;;
+2)
+    Bitz_command
+    ;;
+*)
+    echo -e "${BOLD}${RED} 번호 잘못 입력하신 듯... ㅎㅎ 다시 실행하시면 됩니다 ㅎㅎ${NC}"
+    ;;
+esac
